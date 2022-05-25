43e90d8eda0ddbcd05499d07f3e1fb66ad4e0348
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2022-05-25 22:41:49 +0200

    blank project

---
+++ UnrealGridTest/Source/UnrealGridTest/UnrealGridTestGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "UnrealGridTestGameModeBase.h"
+
---
+++ UnrealGridTest/Source/UnrealGridTest/UnrealGridTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealGridTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGRIDTEST_API AUnrealGridTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
